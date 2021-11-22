489386e8fbc041d04aa6e01218878a56c1ce7202
douziouba <dongliangfe@gmail.com>
2021-11-22 13:41:44 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '3932e951f4c348f38082ef0617c51aa2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
+  name: 'dongliang',
+  domain: 'https://liangdong.notion.site/3932e951f4c348f38082ef0617c51aa2',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
